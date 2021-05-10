b84ff27dbd068efce0314ecf74bd5993f87da890
Santiago Cano <2704@holbertonschool.com>
2021-05-09 19:59:38 -0500

    pep8

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for tens in range(10):
     for ones in range(tens + 1, 10):
         if tens == 8 and ones == 9:
-            print("{}{}".format(tens,ones))
+            print("{}{}".format(tens, ones))
         else:
             print("{}{}, ".format(tens, ones), end='')
