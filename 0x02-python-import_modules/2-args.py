7d4ea2997260cffe7b24af81fe6d56ea6f97ab98
Santiago Cano <2704@holbertonschool.com>
2021-05-11 20:56:33 -0500

    first commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv)
+
+    if i == 1:
+        print("1 argument:")
+    elif i == 0:
+        print("0 arguments:")
+    elif i > 1:
+        print("{} arguments:".format(i - 1))
+    for x in range(1, i):
+        print("{}: {}".format(x, sys.argv[x]))
