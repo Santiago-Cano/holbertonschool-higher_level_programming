d5e27847ef3a305413f690905d690c6e856f8bd7
Santiago Cano <2704@holbertonschool.com>
2021-05-11 21:00:30 -0500

    account for ./2-args.py argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
     i = len(sys.argv)
 
-    if i == 1:
+    if i == 2:
         print("1 argument:")
-    elif i == 0:
+    elif i == 1:
         print("0 arguments:")
-    elif i > 1:
+    elif i > 2:
         print("{} arguments:".format(i - 1))
     for x in range(1, i):
         print("{}: {}".format(x, sys.argv[x]))
