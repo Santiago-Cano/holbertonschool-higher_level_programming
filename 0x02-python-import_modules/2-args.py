c84d551c2419e145ccc3e71c42d0b48350317dab
Santiago Cano <2704@holbertonschool.com>
2021-05-11 21:03:34 -0500

    make everything miles easier to read, fix typo in 0 args case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    i = len(sys.argv)
+    i = len(sys.argv) - 1
 
-    if i == 2:
+    if i == 1:
         print("1 argument:")
-    elif i == 1:
-        print("0 arguments:")
-    elif i > 2:
-        print("{} arguments:".format(i - 1))
-    for x in range(1, i):
+    elif i == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(i))
+    for x in range(1, i + 1):
         print("{}: {}".format(x, sys.argv[x]))
