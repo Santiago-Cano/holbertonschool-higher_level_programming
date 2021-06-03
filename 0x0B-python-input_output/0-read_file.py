2fd96d2889bacacdedf090183875f703902edc5f
Santiago Cano <2704@holbertonschool.com>
2021-06-02 21:30:37 -0500

    pep8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,8 @@ Module 0-read_file
 
 Read a file and print its contents to stdout
 """
+
+
 def read_file(filename=""):
     """Read a file and print its contents to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
