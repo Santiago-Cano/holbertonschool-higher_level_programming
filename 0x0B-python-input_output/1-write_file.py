5de5b343cfdb5e82b094beade05202d4bd7cde7f
Santiago Cano <2704@holbertonschool.com>
2021-06-02 23:18:59 -0500

    first commit

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Module 1-write_file
+
+Write a string to a text file and return the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a text file, return number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
