053896a5d3903b26fdef327474e148389e69c4fb
Santiago Cano <2704@holbertonschool.com>
2021-06-02 23:51:26 -0500

    first commit

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 3-to_json_string
+
+Return JSON representation of an object (string)
+"""
+
+
+def to_json_string(my_obj):
+    """ Return JSON representation of my_obj """
+    import json
+
+    return json.dumps(my_obj)
