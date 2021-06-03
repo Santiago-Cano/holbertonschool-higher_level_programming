ad73fa97080f02bd4c92032aaf842ba95214fd9a
Santiago Cano <2704@holbertonschool.com>
2021-06-02 21:29:34 -0500

    add encoding argument

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ Read a file and print its contents to stdout
 """
 def read_file(filename=""):
     """Read a file and print its contents to stdout"""
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
