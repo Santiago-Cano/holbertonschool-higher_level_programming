68a495c84299b2aff10e699807cd7ba3e5231873
Santiago Cano <2704@holbertonschool.com>
2021-07-12 23:20:27 -0500

    remove quotes on columns

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- This script inserts a new row in first_table from htbn_0c_0
 
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, 'Holberton School');
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
