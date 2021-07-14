a963ab785b882efa21d72f91479ba398d7e82149
Santiago Cano <2704@holbertonschool.com>
2021-07-13 23:25:45 -0500

    add task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- This script creates database hbtn_0d_2 and the user user_0d_2
+
+CREATE DATABASE IF NOT EXISTS htbn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
