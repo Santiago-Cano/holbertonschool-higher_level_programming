fab135709a6824190962433ed199005f9f306551
Santiago Cano <2704@holbertonschool.com>
2021-07-23 14:46:43 -0500

    add tasks 0 - 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""1-square
+A square class with an empty size private attribute
+"""
+
+class Square:
+    """ Class Square
+
+     Args:
+        size: Size of a square
+    """
+    def __init__(self, size):
+        """ Initializes square object and size
+
+        Attributes:
+            __size: Size of a square
+        """
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" 2-square
+Defines class Square with private attribute size
+"""
+class Square:
+    """ Class Square
+
+    Args:
+        size: size of a square
+    """
+    def __init__(self, size=0):
+        """ Initialization of Square
+
+        Attributes:
+            __size: size of a square, defaults to 0 if not passed an argument
+        """
+        if type(size)is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
