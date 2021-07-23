e3142ec330bc0c1e5610d9da327e05136658f61c
Santiago Cano <2704@holbertonschool.com>
2021-07-23 15:22:06 -0500

    add task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+""" 3-square
+Defines class Square with private attribute size and public attribute area
+"""
+
+
+class Square:
+    """ Class Square
+
+    Args:
+        size: size of a square
+    """
+
+    def __init__(self, size=0):
+        """ Initialization of a Square
+
+        Attributes:
+            __size: size of a square, defaults to 0 if no args
+        """
+
+        if type(size)is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Calculates area of a square
+
+        Returns:
+            Area of a square
+        """
+        return (self.__size ** 2)
