33d86c9630d68b7b0565e96113f254b0490c8f46
Santiago Cano <2704@holbertonschool.com>
2021-07-23 17:07:54 -0500

    follow pep8 guidelines

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -45,6 +45,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """ Calculates area of a square
         Returns:
