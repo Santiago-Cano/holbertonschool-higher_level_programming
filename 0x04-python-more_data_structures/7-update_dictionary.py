82a392a51075c0a9080d195f90497ae873d23772
Santiago-Cano <78014751+Santiago-Cano@users.noreply.github.com>
2021-09-09 09:39:01 -0500

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is None:
-        return None
-    dicappend = {key: value}
-    a_dictionary.update(dicappend)
-    return(a_dictionary)
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    if a_dictionary is None:
-        return None
-    newdict = {}
-    for key in a_dictionary:
-        newdict[key] = a_dictionary[key] * 2
-    return (newdict)
