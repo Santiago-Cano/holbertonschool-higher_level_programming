293970897e55093cb862e0cd5a25e615d9b8f162
Santiago Cano <2704@holbertonschool.com>
2021-09-15 23:19:20 -0500

    check for dictionary, then return maximum value key in one line

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) is 0:
         return None
-    name = next(iter(a_dictionary.keys()))
-    high = next(iter(a_dictionary.values()))
-    for index in a_dictionary:
-        if a_dictionary[index] > high:
-            high = a_dictionary[index]
-            name = index
-    return (name)
+    return (max(a_dictionary, key=a_dictionary.get))
