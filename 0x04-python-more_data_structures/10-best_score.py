63f0e78559b990efa0bade77124cd160ec55cde3
Santiago Cano <2704@holbertonschool.com>
2021-09-15 21:55:27 -0500

    hfgujsfdhgnm,

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
         return None
     name = next(iter(a_dictionary.keys()))
     high = next(iter(a_dictionary.values()))
-    for index in a_dictionary.keys():
+    for index in a_dictionary:
         if a_dictionary[index] > high:
             high = a_dictionary[index]
             name = index
