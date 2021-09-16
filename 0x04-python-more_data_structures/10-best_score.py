fb2824095dc72cf2fe84fbfefe330ef7aa1a20ed
Santiago Cano <2704@holbertonschool.com>
2021-09-15 19:42:43 -0500

    work with negative numbers

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) is 0:
         return None
-    name = "hi"
-    high = 0
+    name = next(iter(a_dictionary.keys()))
+    high = next(iter(a_dictionary.values()))
     for index in a_dictionary.keys():
         if a_dictionary[index] > high:
             high = a_dictionary[index]
